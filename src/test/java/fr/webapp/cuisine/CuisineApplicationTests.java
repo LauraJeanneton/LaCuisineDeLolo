1be359a6be64e9a164c481130978d5cd44be40e7
LauraJeanneton <laura.jeanneton@orange.fr>
2022-11-04 14:52:59 +0100

    Initial commit

---
+++ src/test/java/fr/webapp/cuisine/CuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.webapp.cuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuisineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
